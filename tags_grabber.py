31736a2338fd56b0b4c32c3c25823b6406b4a96c
Jake Boraman <jake.boraman@gmail.com>
2023-06-21 15:05:12 +0100

    Initial UI Work

--- tags_grabber.py
+++ tags_grabber.py
@@ -2,54 +2,117 @@ import os
 import sys
 import time
 import zipfile
+import tkinter as tk
+from tkinter import filedialog
 
 def add_file_to_zip(file_path, zip_file):
     zip_file.write(os.path.join(current_directory, 'tags', file_path), arcname=file_path)
+    
+def h3_tag_grab():
+    
+    sevzip_path = os.path.join(current_directory, 'bin\\x64\\7zr.exe') # utilise H3EK's built-in 7zip
+    relative_path = 'reports/' + scen_name + '/cache_file_loaded_tags.txt' # Contains all the referenced tag paths
+    file_path = os.path.join(current_directory, relative_path)
 
+    try:
+        with open(file_path, 'r') as data:
+            text = data.readlines()
+    except FileNotFoundError:
+        print('\nFile not found: ', file_path, '\nDid you build a cache file yet? Please try running the tool again')
+        sys.exit()
+
+    tag_paths = []
+    tags_missing = 0
+
+    for line in text:
+    
+        if line.strip('\n') in tag_paths: # Ignore duplicate entries
+            continue
+    
+        if line[:(line.find('\\') + 1)] in ignore_folders_h3: # Ignore certain folder paths
+            continue
+    
+        # Grab the year from the last modified date on the file, catch exception if tag doesn't exist
+        try:
+            modified_timestamp = os.path.getmtime(os.path.join(current_directory, 'tags', line.strip('\n')))
+            year = time.gmtime(modified_timestamp).tm_year
+        except FileNotFoundError:
+            print('\nMissing tag: ' + line.strip('\n'))
+            tags_missing += 1
+            continue
+    
+        if year == 2007: # Tag is from original EK, ignore
+            continue
+    
+        tag_paths.append(line.strip('\n'))
+    
+    zip_file_path = os.path.join(current_directory, (scen_name + '.zip'))
+    with zipfile.ZipFile(zip_file_path, 'w') as zip_file:
+        for tag in tag_paths:
+            add_file_to_zip(tag, zip_file)
+        
+    print('\nTags successfully zipped - ' + str(tags_missing) + ' missing tags')
+
+# -------------------- Program enters here -----------------------------
+
+# Variables
 # Tags that have any of these strings at the start of their filepath will be ignored
-ignore_folders = {'sound\\', 'globals\\', 'shaders\\', 'fx\\', 'ai\\', 'cinematics\\', 'rasterizer\\', 'ui\\', 'camera\\', 'effects\\'}
+ignore_folders_h2 = {'sound\\', 'sound_remastered\\,' 'globals\\', 'shaders\\', 'ai\\', 'cinematics\\', 'rasterizer\\', 'ui\\', 'camera\\', 'effects\\'}
+ignore_folders_h3 = {'sound\\', 'globals\\', 'shaders\\', 'fx\\', 'ai\\', 'cinematics\\', 'rasterizer\\', 'ui\\', 'camera\\', 'effects\\'}
+
+# UI Functions
+def browse_folder(folder):
+    folder_path = filedialog.askdirectory()
+    folder.config(state='normal')
+    folder.delete(0, tk.END)
+    folder.insert(tk.END, folder_path)
+    folder.config(state='readonly')
+
+# Window creation
+window = tk.Tk()
+window.title("MCC Scenario Tags Zipper")
+window.geometry('450x400')
+engine = tk.StringVar(value='Halo 2')
+engine_label = tk.Label(window, text="Choose engine type:")
+engine_label.grid(row=0, column=1, padx=5, pady=5)
+
+# Engine selection
+engine_options = ['Halo 2', 'Halo 3']
+engine_dd = tk.OptionMenu(window, engine, *engine_options)
+engine_dd.grid(row=1, column=1, padx=5, pady=5)
+
+# Get editing kit location
+folder_label = tk.Label(window, text='Select editing kit root folder:')
+folder_label.grid(row=2, column=1, padx=5, pady=5)
+ek_entry = tk.Entry(window, width=40, state='readonly')
+ek_entry.grid(row=3, column=1, padx=20, pady=5)
+browse_ek_button = tk.Button(window, text='Browse', command=lambda: browse_folder(ek_entry))
+browse_ek_button.grid(row=3, column=2, padx=5, pady=5)
+
+# Get scenario name
+scenario_label = tk.Label(window, text='Type your scenario filename (no extension):')
+scenario_label.grid(row=4, column=1, padx=5, pady=5)
+scenario_field = tk.Entry(window, width=40)
+scenario_field.grid(row=5, column=1, padx=20, pady=5)
+
+# Get output folder
+output_label = tk.Label(window, text='Select an output folder for the zip file:')
+output_label.grid(row=6, column=1, padx=5, pady=5)
+output_entry = tk.Entry(window, width=40, state='readonly')
+output_entry.grid(row=7, column=1, padx=20, pady=5)
+browse_out_button = tk.Button(window, text='Browse', command=lambda: browse_folder(output_entry))
+browse_out_button.grid(row=7, column=2, padx=5, pady=5)
+
+# Go button
+go_label = tk.Button(window, text='Grab all tags!', command=lambda: grabber_initialise())
+go_label.grid(row=8, column=1, padx=50, pady=20)
+
+window.mainloop()
+
+
+
+current_directory = "C:\Program Files (x86)\Steam\steamapps\common\H3EK"
 
 scen_name = input('Enter your scenario file name:\n')
+h3_tag_grab()
 
-current_directory = os.getcwd()
-sevzip_path = os.path.join(current_directory, 'bin\\x64\\7zr.exe') # utilise H3EK's built-in 7zip
-relative_path = 'reports/' + scen_name + '/cache_file_loaded_tags.txt' # Contains all the referenced tag paths
-file_path = os.path.join(current_directory, relative_path)
-
-try:
-    with open(file_path, 'r') as data:
-        text = data.readlines()
-except FileNotFoundError:
-    print('\nFile not found: ', file_path, '\nDid you build a cache file yet? Please try running the tool again')
-    sys.exit()
-
-tag_paths = []
-tags_missing = 0
-
-for line in text:
-    
-    if line.strip('\n') in tag_paths: # Ignore duplicate entries
-        continue
-    
-    if line[:(line.find('\\') + 1)] in ignore_folders: # Ignore certain folder paths
-        continue
-    
-    try:
-        modified_timestamp = os.path.getmtime(os.path.join(current_directory, 'tags', line.strip('\n')))
-        year = time.gmtime(modified_timestamp).tm_year
-    except FileNotFoundError:
-        print('\nMissing tag: ' + line.strip('\n'))
-        tags_missing += 1
-        continue
-    
-    if year == 2007: # Tag is from original EK, ignore
-        continue
-    
-    tag_paths.append(line.strip('\n'))
-    
-zip_file_path = os.path.join(current_directory, (scen_name + '.zip'))
-with zipfile.ZipFile(zip_file_path, 'w') as zip_file:
-    for tag in tag_paths:
-        add_file_to_zip(tag, zip_file)
-        
-print('\nTags successfully zipped - ' + str(tags_missing) + ' missing tags')
